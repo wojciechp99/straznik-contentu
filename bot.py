9d13ea3a8a141b52051fa4a67fb8dbd86528876f
Wojciech <wojciechp99@outlook.com>
2023-10-12 14:10:58 +0200

    fixed bug with not updating json role

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+import math
 import os
 
 import discord
@@ -36,7 +37,7 @@ async def on_message(message):
             with open(path, 'r') as json_file:
                 user = json.load(json_file)
                 json_file.close()
-            await channel.send(f"Ranga: {guild.get_role(user['role']).name}\nCzas contentu: {round(user['hours'], 3)}h")
+            await channel.send(f"Ranga: {guild.get_role(user['role']).name}\nCzas contentu: {math.floor(user['hours'])}h")
         else:
             await channel.send('Niestety nie posiadam twoim danych, co oznacza, że NIE DAWAŁEŚ/AŚ CONTENTU!!')
 
@@ -81,10 +82,11 @@ async def update_user(path, member, stream_start, stream_end):
         hours = count_hours(updated_user['stream_start'], updated_user['stream_end'])
         updated_user['hours'] += hours
 
-        await update_role_id(member, updated_user['hours'], updated_user['role'])
+        new_role = await update_role_id(member, updated_user['hours'], updated_user['role'])
 
         updated_user['stream_start'] = None
         updated_user['stream_end'] = None
+        updated_user['role'] = new_role
 
     with open(path, 'w', encoding='utf-8') as final_json_file:
         json.dump(updated_user, final_json_file, indent=4)
@@ -123,16 +125,17 @@ async def update_role_id(member, user_hours, user_current_role):
         if user_current_role != roles[9]['id']:
             await member.add_roles(guild.get_role(roles[9]['id']), reason="RANK LEVEL UP")
             await member.remove_roles(guild.get_role(user_current_role), reason="RANK LEVEL UP")
-        else:
-            return
 
+    new_user_role = user_current_role
     for index, _ in enumerate(roles):
         if roles[index]['hours'] < user_hours < roles[index + 1]['hours']:
             if user_current_role != roles[index]['id']:
-                await member.add_roles(guild.get_role(roles[index]['id']), reason="RANK LEVEL UP")
+                new_user_role = roles[index]['id']
                 await member.remove_roles(guild.get_role(user_current_role), reason="RANK LEVEL UP")
         else:
             await member.remove_roles(guild.get_role(roles[index]['id']))
+    await member.add_roles(guild.get_role(new_user_role))
+    return new_user_role
 
 
 @client.event
